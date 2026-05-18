8e8564d412d9209f3474c63b60965c04aa75d41e
jasontoups <me@jasontoups.com>
2026-05-17 21:53:41 -0500

    fixing page layout

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -73,7 +73,7 @@ export default async function Post({ params: paramsPromise }: Args) {
           {/* Sidebar: hidden on mobile, flex-grow-1 sticky panel on desktop */}
           <aside
             aria-label="Post sidebar"
-            className="hidden lg:flex lg:flex-col lg:justify-between lg:flex-1 lg:sticky lg:top-0 lg:h-screen py-8 px-6 bg-background"
+            className="hidden w-full lg:flex lg:flex-col lg:justify-between lg:flex-1 lg:sticky lg:top-0 lg:h-screen py-8 px-6 bg-background"
           >
             <SocialShareBar slug={decodedSlug} title={post.title ?? ''} />
             <div className="flex flex-column justify-end">
@@ -90,7 +90,7 @@ export default async function Post({ params: paramsPromise }: Args) {
           {/* Post Body: full-width on mobile, flex-grow-2 on desktop */}
 
           <div className="lg:flex:2 w-full py-8 px-10 bg-white dark:bg-black">
-            <div className="flex justify-start">
+            <div className="flex justify-center">
               <div className="max-w-3xl">
                 <RichText data={post.content} enableGutter={false} />
               </div>
